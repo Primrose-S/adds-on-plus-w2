38cef05f349f0798f47d22a8d0a7f748d0c970e1
Primrose-S <primrosedube71@gmail.com>
2024-09-14 14:39:10 +0200

    added javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,21 @@
-alert("hello primy");
+function updateTime (){
+let newYorkElement = document.querySelector("#new-york");
+let newYorkDateElement =  document.querySelector(".date");
+let newYorkTimeElement =  document.querySelector(".time");
+let newYorkTime = moment().tz("America/new_york");
+newYorkDateElement.innerHTML = newYorkTime.format("dddd, MMMM Do YYYY");
+newYorkTimeElement.innerHTML = `${newYorkTime.format("h:mm:ss")} <small>${newYorkTime.format("a")}</small>`
+
+//setInterval(updateNewYorskTime, 1000);
+
+//lisbon
+//function updateLisbonTime () {
+let lisbonElement = document.querySelector("#lisbon");
+let lisbonDateElement =  document.querySelector(".dates");
+let lisbonTimeElement =  document.querySelector(".times");
+let lisbonTime = moment().tz("Europe/Lisbon");
+lisbonDateElement.innerHTML = lisbonTime.format("dddd, MMMM Do YYYY");
+lisbonTimeElement.innerHTML = `${lisbonTime.format("h:mm:ss")} <small>${lisbonTime.format("A")}</small>`
+
+}
+setInterval(updateTime, 1000);
