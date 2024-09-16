d99b9055bb83b1dcb7ee7be39d4087a150d63763
Primrose-S <primrosedube71@gmail.com>
2024-09-16 23:36:18 +0200

    improving

--- javascript/index.js
+++ javascript/index.js
@@ -26,7 +26,11 @@ newYorkTimeElement.innerHTML = `${newYorkTime.format("H:mm:ss")} <small>${newYor
 
  
 function updateCity(event) {
+
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess()
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
